c694c77d8187bc4dbaaead84008fdec55eb12e02
Mohammad Soqar <mnsoqar1@gmail.com>
2024-03-16 13:40:15 +0300

    Update user_provider.dart

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:localink_sm/models/user.dart';
+import 'package:localink_sm/models/user.dart' as model;
 import 'package:localink_sm/resources/auth_methods.dart';
 
 class UserProvider with ChangeNotifier {
@@ -9,10 +10,15 @@ class UserProvider with ChangeNotifier {
   User? get getUser => _user;
 
   Future<void> refreshUser() async {
+  try {
     User user = await _authMethods.getUserDetails();
     _user = user;
     notifyListeners();
+  } catch (e) {
+    print("Error refreshing user: $e");
   }
+}
+
 
   
 }
