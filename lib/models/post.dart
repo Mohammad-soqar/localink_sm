a478d55b47967cf5b5646b7fe644b0c13bac895d
Mohammad Soqar <mnsoqar1@gmail.com>
2024-03-16 13:40:07 +0300

    Update post.dart

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,52 +1,51 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Post {
-  final String description;
-  final String profileImage;
-  final String username;
+  final String id;
   final String uid;
-  final String postId;
-  final String postUrl;
-  final DateTime datePublished;
-  // Users who liked the post
-  final List<dynamic> likes;
+  final Timestamp createdDatetime;
+  final String caption;
+  final DocumentReference postType;
+  final String longitude;
+  final String latitude;
+  final String locationName;
   final List<dynamic> hashtags;
 
   const Post(
-      {required this.description,
+      {required this.id,
       required this.uid,
-      required this.username,
-      required this.profileImage,
-      required this.postId,
-      required this.datePublished,
-      required this.postUrl,
-      required this.likes,
+      required this.createdDatetime,
+      required this.caption,
+      required this.postType,
+      required this.longitude,
+      required this.latitude,
+      required this.locationName,
       required this.hashtags});
 
   static Post fromSnap(DocumentSnapshot snap) {
     var snapshot = snap.data() as Map<String, dynamic>;
     return Post(
-      username: snapshot['username'],
+      id: snapshot['id'],
       uid: snapshot['uid'],
-      description: snapshot['description'],
-      profileImage: snapshot['profileImage'],
-      postId: snapshot['postId'],
-      datePublished: snapshot['datePublished'],
-      postUrl: snapshot['postUrl'],
-      likes: (snapshot['likes'] as List<dynamic>).cast<String>(),
+      createdDatetime: snapshot['createdDatetime'],
+      caption: snapshot['caption'],
+      postType: snapshot['postType'],
+      longitude: snapshot['longitude'],
+      latitude: snapshot['latitude'],
+      locationName: snapshot['locationName'],
       hashtags: (snapshot['likes'] as List<dynamic>).cast<String>(),
     );
   }
 
   Map<String, dynamic> toJson() => {
-        "description": description,
-        "username": username,
+        "id": id,
         "uid": uid,
-        "profileImage": profileImage,
-        "postId": postId,
-        "datePublished": datePublished,
-        "postUrl": postUrl,
-        "likes": likes,
+        "createdDatetime": createdDatetime,
+        "caption": caption,
+        "postType": postType,
+        "longitude": longitude,
+        "latitude": latitude,
+        "locationName": locationName,
         "hashtags": hashtags,
       };
 }
