aaaa2528e3393d0a5da6bc2104c8ee804e073800
Mohammad Soqar <mnsoqar1@gmail.com>
2025-07-01 01:21:29 +0300

    Migrate to mapbox_maps_flutter and update dependencies
    Replaces mapbox_gl with mapbox_maps_flutter in new_map_picker.dart and updates related logic to use the new API. Updates pubspec.yaml to use mapbox_maps_flutter and geocoding 4.x. Upgrades Gradle wrapper to 8.3 and adjusts Android build files for compatibility. Removes custom VSCode color settings.

--- lib/widgets/new_map_picker.dart
+++ lib/widgets/new_map_picker.dart
@@ -1,122 +1,124 @@
 import 'dart:async';
 import 'dart:math';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'package:localink_sm/utils/RestrictedAreasService.dart';
-import 'package:mapbox_gl/mapbox_gl.dart';
+
+class LatLng {
+  final double latitude;
+  final double longitude;
+
+  LatLng(this.latitude, this.longitude);
+}
 
 class MapPickerScreen extends StatefulWidget {
   final Function(LatLng) onLocationPicked;
 
-
   const MapPickerScreen({Key? key, required this.onLocationPicked}) : super(key: key);
 
   @override
-  _MapPickerScreenState createState() => _MapPickerScreenState();
+  State<MapPickerScreen> createState() => _MapPickerScreenState();
 }
 
 class _MapPickerScreenState extends State<MapPickerScreen> {
-  MapboxMapController? _controller;
-  LatLng? _pickedLocation;
-  Completer<MapboxMapController> _controllerCompleter =
-      Completer<MapboxMapController>();
+  late MapboxMap _mapboxMap;
+  late CircleAnnotationManager _circleManager;
+  late PointAnnotationManager _pointManager;
+
   List<LatLng> _restrictedAreas = [];
+  LatLng? _pickedLocation;
+
+  final GlobalKey _mapKey = GlobalKey();
 
   @override
   void initState() {
     super.initState();
-    _setInitialLocation();
-   _fetchRestrictedAreas();
-  }
-
-  void _setInitialLocation() {
-    // Set the initial location here
+    _fetchRestrictedAreas();
   }
 
   Future<void> _fetchRestrictedAreas() async {
-    RestrictedAreasService service = RestrictedAreasService();
+    final service = RestrictedAreasService();
     List<LatLng> areas = await service.fetchRestrictedAreas();
-    setState(() {
-      _restrictedAreas = areas;
-    });
-    _displayRestrictedAreas();
+    setState(() => _restrictedAreas = areas);
   }
 
-  Future<void> _displayRestrictedAreas() async {
-    final controller = await _controllerCompleter.future;
+  Future<void> _addRestrictedAreas() async {
+    if (_circleManager == null) return;
     for (var area in _restrictedAreas) {
-      controller.addCircle(
-        CircleOptions(
-          geometry: area,
-          circleRadius: 8.0,
-          circleColor: "#116640",
-          circleOpacity: 0.5,
-        ),
-      );
+      await _circleManager.create(CircleAnnotationOptions(
+        geometry: Point(coordinates: Position(area.longitude, area.latitude)),
+        circleRadius: 8,
+        circleColor: "#116640",
+        circleOpacity: 0.5,
+      ));
     }
-  } 
-
-  Future<void> _onMapCreated(MapboxMapController controller) async {
-    _controller = controller;
-    _controllerCompleter.complete(controller);
-    await _moveCameraToCurrentLocation();
-    _displayRestrictedAreas();
   }
 
-  Future<void> _moveCameraToCurrentLocation() async {
+  Future<void> _onMapCreated(MapboxMap mapboxMap) async {
+    _mapboxMap = mapboxMap;
+
+    _circleManager = await _mapboxMap.annotations.createCircleAnnotationManager();
+    _pointManager = await _mapboxMap.annotations.createPointAnnotationManager();
+
+    await _addRestrictedAreas();
     if (_pickedLocation != null) {
-      final controller = await _controllerCompleter.future;
-      controller.moveCamera(CameraUpdate.newLatLng(_pickedLocation!));
-      _addSymbolAtLocation(_pickedLocation!);
+      await _moveCameraTo(_pickedLocation!);
+      await _addSymbolAt(_pickedLocation!);
     }
   }
 
-  void _onMapTap(Point<double> point, LatLng coordinates) {
-    if (_isRestrictedArea(coordinates)) {
+  Future<void> _moveCameraTo(LatLng location) async {
+    await _mapboxMap.setCamera(CameraOptions(
+      center: Point(coordinates: Position(location.longitude, location.latitude)),
+      zoom: 14,
+    ));
+  }
+
+  Future<void> _addSymbolAt(LatLng location) async {
+    await _pointManager.deleteAll();
+    await _pointManager.create(PointAnnotationOptions(
+      geometry: Point(coordinates: Position(location.longitude, location.latitude)),
+      image: await _loadImageFromAsset("assets/icons/mapPin2.png"),
+      iconSize: 1.5,
+    ));
+  }
+
+  Future<Uint8List> _loadImageFromAsset(String assetPath) async {
+    final ByteData byteData = await rootBundle.load(assetPath);
+    return byteData.buffer.asUint8List();
+  }
+
+  void _onMapTap(ScreenCoordinate _, LatLng latLng) async {
+    if (_isRestrictedArea(latLng)) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Cannot add events in restricted areas')),
+        const SnackBar(content: Text('Cannot add events in restricted areas')),
       );
       return;
     }
 
-    setState(() {
-      _pickedLocation = coordinates;
-    });
-    _controller?.clearSymbols();
-    _addSymbolAtLocation(coordinates);
+    setState(() => _pickedLocation = latLng);
+    await _addSymbolAt(latLng);
   }
 
-  void _addSymbolAtLocation(LatLng location) async {
-    final controller = await _controllerCompleter.future;
-    if (location != null) {
-      controller.addSymbol(
-        SymbolOptions(
-          geometry: location,
-          iconImage: "assets/icons/mapPin2.png",
-        ),
-      );
-    }
-  }
-
-  bool _isRestrictedArea(LatLng coordinates) {
-    for (LatLng area in _restrictedAreas) {
-      if (_calculateDistance(coordinates, area) < 0.05) {
-        // Distance in kilometers
-        return true;
-      }
+  bool _isRestrictedArea(LatLng location) {
+    for (final area in _restrictedAreas) {
+      if (_calculateDistance(location, area) < 0.05) return true;
     }
     return false;
   }
 
-  double _calculateDistance(LatLng point1, LatLng point2) {
-    var p = 0.017453292519943295; // Math.PI / 180
+  double _calculateDistance(LatLng p1, LatLng p2) {
+    var p = 0.017453292519943295;
     var c = cos;
     var a = 0.5 -
-        c((point2.latitude - point1.latitude) * p) / 2 +
-        c(point1.latitude * p) *
-            c(point2.latitude * p) *
-            (1 - c((point2.longitude - point1.longitude) * p)) /
-            2;
-    return 12742 * asin(sqrt(a)); // 2 * R; R = 6371 km
+        c((p2.latitude - p1.latitude) * p) / 2 +
+        c(p1.latitude * p) *
+            c(p2.latitude * p) *
+            (1 - c((p2.longitude - p1.longitude) * p)) / 2;
+    return 12742 * asin(sqrt(a));
   }
 
   void _onPickLocation() {
@@ -130,25 +132,32 @@ class _MapPickerScreenState extends State<MapPickerScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pick Location'),
+        title: const Text('Pick Location'),
         actions: [
           IconButton(
-            icon: Icon(Icons.check),
+            icon: const Icon(Icons.check),
             onPressed: _onPickLocation,
-          )
+          ),
         ],
       ),
-      body: MapboxMap(
-        accessToken:
-            "sk.eyJ1IjoibW9oYW1tYWRzb3FhcjEwMSIsImEiOiJjbHUyM3Rwc2owc2p6MmtrMWg1eTNjb25oIn0.uP5k1FrxSDNNBjWo1LdSlg",
-        onMapCreated: _onMapCreated,
-        styleString: "mapbox://styles/mapbox/dark-v11",
-        initialCameraPosition: CameraPosition(
-          target: _pickedLocation ??
-              LatLng(40.99437101906267, 28.99925336148795), // Default location if null
+      body: MapWidget(
+        key: _mapKey,
+        resourceOptions: ResourceOptions(
+          accessToken: "sk.eyJ1IjoibW9oYW1tYWRzb3FhcjEwMSIsImEiOiJjbHUyM3Rwc2owc2p6MmtrMWg1eTNjb25oIn0.uP5k1FrxSDNNBjWo1LdSlg",
+        ),
+        styleUri: MapboxStyles.MAPBOX_DARK,
+        cameraOptions: CameraOptions(
+          center: Point(
+            coordinates: Position(
+              _pickedLocation?.longitude ?? 28.99925,
+              _pickedLocation?.latitude ?? 40.99437,
+            ),
+          ),
           zoom: 10.0,
         ),
-        onMapClick: _onMapTap,
+        mapOptions: MapOptions(pixelRatio: MediaQuery.of(context).devicePixelRatio),
+        onMapCreated: _onMapCreated,
+        onTapListener: _onMapTap,
       ),
     );
   }
